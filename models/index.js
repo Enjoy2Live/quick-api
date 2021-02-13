28806ff660ef779b7f00d3d078a823c84073776c
Enjoy2Live <iMohammedMaqbol@gmail.com>
2021-02-14 02:35:33 +0300

    removed console.log

--- models/index.js
+++ models/index.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 // const dotenv = require("dotenv");
 
 const { DB_USER, DB_PASSWORD, DB_NAME } = process.env;
-console.log(DB_USER, DB_PASSWORD, DB_NAME);
+
 const connection = `mongodb+srv://${DB_USER}:${DB_PASSWORD}@cluster0.refbm.mongodb.net/${DB_NAME}?retryWrites=true&w=majority`;
 
 exports.connectToDatabase = () => {
